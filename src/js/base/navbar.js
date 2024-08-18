24613942be7a5734f84e92329f2ee09c60847d71
Reyhaneh-hm <reyhaneh3784@gmail.com>
2024-08-18 13:22:57 +0330

    Add changes

--- src/js/base/navbar.js
+++ src/js/base/navbar.js
@@ -22,3 +22,15 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+
+/*--------------------------------------------*/
+// const filterBtn = document.querySelector("#filterBtn");
+// const modalFilter = document.querySelector(".modalFilter");
+// const overlay = document.querySelector(".overlay");
+// filterBtn.addEventListener("click", () => {
+//     [overlay, modalFilter].forEach((item) => item.classList.add("active"));
+// });
+// overlay.addEventListener("click", () => {
+//     [overlay, modalFilter].forEach((item) => item.classList.remove("active"));
+// });
