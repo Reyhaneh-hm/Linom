8271cfef845d3b87e5d9a9246235ecd6d52221e6
Reyhaneh-hm <reyhaneh3784@gmail.com>
2024-08-18 11:09:58 +0330

    Add filter to page list-blog

--- src/js/pages/list-blog/listBlog.js
+++ src/js/pages/list-blog/listBlog.js
@@ -1,3 +1,13 @@
 import "../../../sass/components/pages/list-blog/index.scss";
 import "../../../sass/base/responsiv.scss"
 /*------------------------------------------------*/
+
+const filterBtn = document.querySelector("#filterBtn");
+const modalFilter = document.querySelector(".modalFilter");
+const overlay = document.querySelector(".overlay");
+filterBtn.addEventListener("click", () => {
+    [overlay, modalFilter].forEach((item) => item.classList.add("active"));
+});
+overlay.addEventListener("click", () => {
+    [overlay, modalFilter].forEach((item) => item.classList.remove("active"));
+});
