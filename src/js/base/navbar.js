a888c2ce241dcf399e6ee1276368ce0af16b964f
Reyhaneh-hm <reyhaneh3784@gmail.com>
2024-09-15 11:35:35 +0330

    Making the bug Social Network

--- src/js/base/navbar.js
+++ src/js/base/navbar.js
@@ -42,9 +42,15 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
 /*-------------------------Social Network-----------------------------*/
-
 const icons = ['icon-smallCircle icon-Telegram', 'icon-smallCircle icon-Instagram', 'icon-smallCircle icon-Facebook', 'icon-smallCircle icon-Whatsapp', 'icon-smallCircle icon-Twitter'];
 const colors = ['Telegram', 'Instagram', 'Facebook', 'Whatsapp', 'Twitter'];
+const positions = [
+    { x: -20, y: -75 },
+    { x: 30, y: -80 },
+    { x: 71, y: -53 },
+    { x: 84, y: -5 },
+    { x: 70, y: 44 }
+];
 const centerCircle = document.getElementById('centerCircle');
 
 colors.forEach((color, index) => {
@@ -53,32 +59,29 @@ colors.forEach((color, index) => {
     smallCircle.classList.add('small-circle', `circle-${color}`);
     smallCircle.innerHTML = `<i class="${icons[index]}"></i>`;
     centerCircle.appendChild(smallCircle);
+
+    smallCircle.addEventListener('mouseover', () => {
+        smallCircle.style.transform += ' scale(1.12)';
+    });
+
+    smallCircle.addEventListener('mouseout', () => {
+        smallCircle.style.transform = smallCircle.style.transform.replace(' scale(1.12)', '');
+    });
 });
 
 let isExpanded = false;
 const delay = 150;
-centerCircle.style.zIndex = '61';
-
 
 centerCircle.addEventListener('click', () => {
     const smallCircles = document.querySelectorAll('.small-circle');
-    const circleTelegram = document.querySelector('.circle-Telegram');
-    const circleInstagram = document.querySelector('.circle-Instagram');
-    const circleFacebook = document.querySelector('.circle-Facebook');
-    const circleWhatsapp = document.querySelector('.circle-Whatsapp');
-    const circleTwitter = document.querySelector('.circle-Twitter');
-
+    
     if (!isExpanded) {
         smallCircles.forEach((circle, index) => {
             setTimeout(() => {
-                circleTelegram.style.transform = "translate(-20px,-75px)";
-                circleInstagram.style.transform = "translate(30px,-80px)";
-                circleFacebook.style.transform = "translate(71px,-53px)";
-                circleWhatsapp.style.transform = "translate(84px,-5px)";
-                circleTwitter.style.transform = "translate(70px,44px)";
-
+                const position = positions[index];
+                circle.style.transform = `translate(${position.x}px, ${position.y}px)`;
                 circle.style.opacity = '1';
-                circle.style.zIndex = '-59';
+                circle.style.zIndex = '60';
             }, index * delay);
         });
         isExpanded = true;
@@ -86,15 +89,9 @@ centerCircle.addEventListener('click', () => {
     } else {
         smallCircles.forEach((circle, index) => {
             setTimeout(() => {
+                circle.style.transform = 'translate(0px, 0px)';
                 circle.style.opacity = '0';
                 circle.style.zIndex = '-60';
-
-                circleTelegram.style.transform = "translate(0,0)";
-                circleInstagram.style.transform = "translate(0,0)";
-                circleFacebook.style.transform = "translate(0,0)";
-                circleWhatsapp.style.transform = "translate(0,0)";
-                circleTwitter.style.transform = "translate(0,0)";
-
             }, index * delay);
         });
         isExpanded = false;
