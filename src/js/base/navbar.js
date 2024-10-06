ae313e6eb31bbec67594c5730a05629213f5574e
Reyhaneh-hm <reyhaneh3784@gmail.com>
2024-10-06 19:37:32 +0330

    Responsive second section and Add footer and Add filterProduct

--- src/js/base/navbar.js
+++ src/js/base/navbar.js
@@ -1,5 +1,6 @@
 import "../../scss/components/public/navbar.scss";
-/*-------------------------------------------*/
+
+/*---------------------Logo----------------------*/
 
 window.addEventListener("DOMContentLoaded", () => {
     const menu = document.querySelector(".menu");
@@ -19,5 +20,24 @@ window.addEventListener("DOMContentLoaded", () => {
 
             }
         }
+    });
+});
+
+/*-----------------------show filterProduct------------------------*/
+
+window.addEventListener("DOMContentLoaded", () => {
+    const checkbox = document.querySelector("#checkbox");
+    const filterProduct = document.querySelector(".filterProduct");
+    const overlay = document.querySelector(".overlay");
+
+    checkbox.addEventListener("change", () => {
+        filterProduct.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+
+    overlay.addEventListener("click", () => {
+        filterProduct.classList.remove("active");
+        overlay.classList.remove("active");
+        checkbox.checked = false;  // میاد وضعیت رو ریست میکنه
     });
 });
